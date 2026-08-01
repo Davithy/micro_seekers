1d05266237031219a03322054aa3506348876fc2
Davithy <davithy00@gmail.com>
2026-07-31 22:55:39 -0700

    Gaze click
    Added idle counter to trigger a gaze ray cast and a cross hair to indicate center of the screen

--- Assets/Scripts/Controller/InputController.cs
+++ Assets/Scripts/Controller/InputController.cs
@@ -1,11 +1,23 @@
 using Interfaces;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace Controller
 {
     public class InputController : MonoBehaviour
     {
+        [Header("Idle Time")]
+        [SerializeField, RangeAttribute(1f, 5f)] private float gazeTimer = 3f;
+
+        [Header("Camera Input")]
+        [SerializeField] private InputActionReference moveAction;
+        [SerializeField] private InputActionReference rotateAction;
+
         private Camera camera;
+        private float idleTimer;
+        private bool hasTriggered;
+        private bool isIdle;
+        
         private void Awake()
         {
             camera = Camera.main;
@@ -13,22 +25,41 @@ namespace Controller
 
         private void Update()
         {
-            // if (Application.platform == RuntimePlatform.WindowsEditor)
-            // {
-            //     if (!Input.GetMouseButtonDown(0)) return;
-            //     Ray ray = camera.ScreenPointToRay(Input.mousePosition);
-            //     if (!Physics.Raycast(ray, out RaycastHit hitData)) return;
-            //     HandleClickable(hitData);
+            Vector2 move = moveAction.action.ReadValue<Vector2>();
+            float rotate = rotateAction.action.ReadValue<float>();
 
-            // }
+            if (move == Vector2.zero && rotate == 0f) { isIdle = true; } 
+            else { isIdle = false; }
+
+            if (!isIdle)
+            {
+                idleTimer = 0f;
+                hasTriggered = false;
+                return;
+            }
+
+            idleTimer += Time.deltaTime;
+
+            if (idleTimer >= gazeTimer && !hasTriggered)
+            {
+                hasTriggered = true;
+                RayCaster();
+            }
+        }
+
+        private void RayCaster()
+        {
+            Ray ray = new Ray(camera.transform.position, camera.transform.forward);
+            if (!Physics.Raycast(ray, out RaycastHit hitData)) return;
+            HandleClickable(hitData);
         }
 
         private void HandleClickable(RaycastHit hitData)
         {
-            // if (hitData.transform.TryGetComponent<iClickable>(out iClickable clickable))
-            // {
-            //     clickable.OnClick();   
-            // }
+            if (hitData.transform.TryGetComponent<iClickable>(out iClickable clickable))
+            {
+                clickable.OnClick();   
+            }
         }
     }
 }
